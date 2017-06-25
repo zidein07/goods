ef8081c450053121b7007fe3771916597c776754
zidein <zidein07@gmail.com>
2017-06-26 00:28:00 +0300

    update good

--- app/scripts/dev/config/translate.config.js
+++ app/scripts/dev/config/translate.config.js
@@ -19,7 +19,8 @@ GoodsApp.config(['$translateProvider', function ($translateProvider) {
     // TODO: Можно обработать ui-sref с помощью $compile затем вывести с помощью trustAsHtml, но а пока оставим href
     'CART_EMPTY_DESCRIPTION': '<p>Your cart is empty, but you can buy <b>luke skywalker`s ship</b> redirect to this <a href="#/">page</a></p>',
     'CONFIRM_AN_ORDER': 'Confirmation of an order',
-    'LOADING': 'Loading'
+    'LOADING': 'Loading',
+    'DESCRIPTION_OF_SPACESHIP':'<p>Description of the spaceship</p>'
   }).translations('ru', {
     'PARSER_TITLE': 'Парсинг стены',
     'SHIPS': 'Космические корабли',
@@ -39,7 +40,8 @@ GoodsApp.config(['$translateProvider', function ($translateProvider) {
     // TODO: Можно обработать ui-sref с помощью $compile затем вывести с помощью trustAsHtml, но а пока оставим href
     'CART_EMPTY_DESCRIPTION': '<p>Ваша корзина пуста, но вы можете приобрести <b>космический корабль Люка Скайвокера</b> для этого перейдите на эту <a href="#/">страницу</a></p>',
     'CONFIRM_AN_ORDER': 'Подтверждение заказа',
-    'LOADING': 'Загрузка'
+    'LOADING': 'Загрузка',
+    'DESCRIPTION_OF_SPACESHIP':'<p>Какое-то описание об этом корабле</p>'
   }).registerAvailableLanguageKeys(['en', 'ru'], {
     'en_*': 'en',
     'ru_*': 'ru',
